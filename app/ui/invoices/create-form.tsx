cd41178bce01ec054214ed441cb43af4f965285a
binhth <binhth@hocmai.vn>
2023-11-21 17:11:40 +0700

    fix: deploy production enviroment code

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -45,7 +45,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
           <div id="customer-error" aria-live="polite" aria-atomic="true">
-            {state.errors.customerId &&
+            {state?.errors?.customerId &&
               state.errors.customerId.map((error: string) => (
                 <p className="mt-2 text-sm text-red-500" key={error}>
                   {error}
@@ -73,7 +73,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
-          {state.errors.amount &&
+          {state?.errors?.amount &&
             state.errors.amount.map((error: string) => (
               <p className="mt-2 text-sm text-red-500" key={error}>
                 {error}
@@ -120,7 +120,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               </div>
             </div>
           </div>
-          {state.errors.status &&
+          {state?.errors?.status &&
             state.errors.status.map((error: string) => (
               <p className="mt-2 text-sm text-red-500" key={error}>
                 {error}
